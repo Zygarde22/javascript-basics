cafcf4c4a127aa80a724fd5c59b22826505c8a67
Khris R <khristonrobinson@gmail.com>
2025-02-18 19:40:33 -0600

    added bracket and dot notation

--- objects.js
+++ objects.js
@@ -2,3 +2,13 @@ const person= {
     name: "Khriston",
     age: 29,
 };
+
+console.log(person.name);
+console.log(person.age);
+
+person["name"] = "Khris";
+person["age"] = "30";
+
+console.log(person["name"]); 
+console.log(age ["age"]);
+
