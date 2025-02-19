538f0a908c93bbba50b90fc29cc0a5ae7eee23ae
Khris R <khristonrobinson@gmail.com>
2025-02-18 19:43:35 -0600

    added more code string

--- objects.js
+++ objects.js
@@ -10,5 +10,22 @@ person["name"] = "Khris";
 person["age"] = "30";
 
 console.log(person["name"]); 
-console.log(age ["age"]);
+console.log(person["age"]);
 
+
+const person = {
+    name: "Khriston",
+    age: 29,
+    job: "Developer"
+  };
+  
+  // Using a variable to dynamically access properties
+  let property = "name";
+  console.log(person[property]);  // Prints "Khriston"
+  
+  property = "age";
+  console.log(person[property]);  // Prints 29
+  
+  property = "job";
+  console.log(person[property]);  // Prints "Developer"
+  
